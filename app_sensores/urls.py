8da2e79c5d58bb7d220d6c0f44917a49cbb997ca
murilopbc <murilopoltronieri3@gmail.com>
2024-05-09 16:49:57 -0300

    django

--- app_sensores/urls.py
+++ app_sensores/urls.py
@@ -4,5 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.abrir_index, name='abrir_index'),
     path('listar_dados', views.listar_dados, name='listar_dados'),
+    path('editar_dados', views.editar_dados, name='editar_dados'),
+    path('excluir_dados/<int:id>', views.excluir_dados, name='excluir_dados'),
+    path('abrir_edicao/<int:id>',views.abrir_edicao, name='abrir_edicao'),
 
+
+    
 ]
--- app_sensores/views.py
+++ app_sensores/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Pessoas
 
 def abrir_index(request):
@@ -24,3 +25,30 @@ def listar_dados(request):
     
     return render(request, 'index.html', {'mensagem': mensagem, 'dados': dados, 'nome': nome, 'idade': idade})
 
+
+def excluir_dados(request, id):
+    pessoa  = get_object_or_404(Pessoas, id=id)
+    print(pessoa)
+    pessoa.delete()
+    return abrir_index(request)
+
+def abrir_edicao(request, id):
+    pessoas = Pessoas.objects.filter(id=id)
+    nome = pessoas[0].nome
+    idade = pessoas[0].idade
+    return render(request , 'cadastro.html',{"pessoas":pessoas,"nome":nome,"idade":idade,"id":id} )
+
+
+def editar_dados(request):
+    nome = request.POST.get('nome')
+    idade = request.POST.get('idade')
+    id = request.POST.get('id')
+
+    pessoa = get_object_or_404(Pessoas ,id=id)
+
+    pessoa.nome = nome
+    pessoa.idade = idade
+    
+    pessoa.save()     
+    return render (request, 'index.html')
+    
